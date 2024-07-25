15e362edd30d7c3a0915a4df130950fb3fcf2fd7
Pranav Agarwal <pranavagarwal@Pranavs-MacBook-Air.local>
2024-07-25 16:54:07 +0530

    Added Application API

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ service_account_path = 'lloyds-hack-grp-50-98938c24feeb.json'
 db = firestore.Client.from_service_account_json(service_account_path)  
 
 app = FastAPI()
+
+
 def login_user(email,password):
     user_doc = db.collection('users').document('evk7p33pLRG0HjxZ12lK')
     doc = user_doc.get()
@@ -23,11 +25,48 @@ def login_user(email,password):
             detail="Invalid User",
             headers={"Custom-Header": "CustomHeaderValue"}
         )
+
+@app.get('/applications')
+def application_data(user_id:str):
+    print("user_id: "+user_id)
+    return get_application_data(user_id)
+
+def get_application_data(user_id):
+    collection_name='applications'
+    field_name = 'user_id'
+    collection_ref = db.collection(collection_name)
+    query = collection_ref.where(field_name,"==",user_id)
+    # Execute the query and get the results
+    # results = query.stream()
+    # Print the results
+    value =  query.stream()
+    app_list = []
+    for val in value:
+        app_list.append(val.to_dict())
+
+    return {"message":app_list}
+    # if not doc.exists:
+    #     return {"error": "User does not exist"}, 400
     
+    # if password == 'abc':
+    #     return doc.to_dict()
+    # else:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_401_UNAUTHORIZED,
+    #         detail="Invalid User",
+    #         headers={"Custom-Header": "CustomHeaderValue"}
+    #     )
+    
+
+
 class UserData(BaseModel):
     email: str
     password: str 
 
-@app.post('/login',)
+# @app.post('')
+
+# @app.put('')
+
+@app.post('/login')
 def login(userData:UserData):
     return login_user(getattr(userData,'email'),getattr(userData,'password'))
